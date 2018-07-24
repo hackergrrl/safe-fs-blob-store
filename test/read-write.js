e506ddb631f741eb2ba515089bbeb738c0ae1753
Gregor MacLennan <gmaclennan@digital-democracy.org>
2018-07-24 17:11:35 -0500

    Breaking test with funky filename

--- test/read-write.js
+++ test/read-write.js
@@ -47,6 +47,47 @@ test('reading a blob as a stream', function (t) {
   })
 })
 
+test('subdirs don\'t conflict with prefixes', function (t) {
+  common.setup(test, function (err, store) {
+    t.notOk(err, 'no setup err')
+    var filenames = ['foobar1filename.txt', 'foobar1', 'foobar1/filename.txt', 'foobar1/filename']
+    var pending = filenames.length
+    filenames.forEach(function (name) {
+      var ws = store.createWriteStream({name: name}, onWrite)
+      from([Buffer.from(name)]).pipe(ws)
+    })
+
+    function onWrite (err, obj) {
+      t.error(err)
+      t.ok(obj.key, 'blob has key')
+      if (--pending > 0) return
+      pending = filenames.length
+      filenames.forEach(function (name) {
+        var rs = store.createReadStream(name)
+
+        rs.on('error', function (e) {
+          t.false(e, 'no read stream err')
+          t.end()
+        })
+
+        rs.pipe(concat(function (file) {
+          t.equal(file.toString(), name, 'correct file is returned')
+          done()
+        }))
+      })
+    }
+
+    function done () {
+      if (--pending > 0) return
+      common.teardown(test, store, null, function (err) {
+        t.error(err)
+        t.end()
+      })
+    }
+  })
+})
+
+
 test('reading a blob that does not exist', function (t) {
   common.setup(test, function (err, store) {
     t.notOk(err, 'no setup err')
